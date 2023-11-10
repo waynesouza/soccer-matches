51de880e3545c7ae590e4236f81962b47eaafaf4
Wayne <waynesouza07@gmail.com>
2023-11-10 15:00:57 -0300

    fix: removida uma propriedade desnecssária no application.yml, movidos os arquivos DTOs para a hierarquia de pasta correta e adicionado o arquivo de configuração do pré commit

--- src/main/java/com/waynesouza/soccer/domain/dto/PartidaDTO.java
+++ src/main/java/com/waynesouza/soccer/domain/dto/PartidaDTO.java
@@ -1,7 +1,8 @@
-package com.waynesouza.soccer.dto;
+package com.waynesouza.soccer.domain.dto;
 
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,22 +12,31 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class PartidaAtualizadaDTO implements Serializable {
+public class PartidaDTO implements Serializable {
 
+    private String id;
+
+    @NotNull(message = "O time mandante deve ser informado")
     private String timeMandante;
 
+    @NotNull(message = "A quantidade de gols do time mandante deve ser informada")
     @Min(value = 0, message = "O valor deve ser um número inteiro maior que zero")
     @Digits(integer = 2, fraction = 0, message = "O valor deve ser um número inteiro positivo ou zero")
     private BigDecimal quantidadeGolMandante;
 
+    @NotNull(message = "O time visitante deve ser informado")
     private String timeVisitante;
 
+    @NotNull(message = "A quantidade de gols do time visitante deve ser informada")
     @Min(value = 0, message = "O valor deve ser um número inteiro maior que zero")
     @Digits(integer = 2, fraction = 0, message = "O valor deve ser um número inteiro positivo ou zero")
     private Integer quantidadeGolVisitante;
 
+    @NotNull(message = "A data com o horário da partida deve ser informada")
     private LocalDateTime dataHora;
 
+    @NotNull(message = "O estádio da partida deve ser informado")
     private String estadio;
+
 
 }
